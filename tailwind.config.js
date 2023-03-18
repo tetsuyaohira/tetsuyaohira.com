edeef3c04d7648723320a2b815d60e33a2a0f15f
tetsuyaohira <tetsuya.ohira@icloud.com>
2023-03-18 15:35:19 +0900

    styleのtailwind css化

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.js",
+    "./components/**/*.js",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
